cf22038837955bce9ecfc9d33251a71867d276f4
Wubeshet Yimam <w.yimam@alustudent.com>
2023-04-16 04:10:49 +0300

    Add footer page

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import Banner from "./components/banner/Banner";
 import About from "./components/about/About";
 import Projects from "./projects/Projects";
 import Resume from "./components/resume/Resume";
-import Contact from "./components/contact/Contact"
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <div className="w-full h-auto bg-bodyColor text-lightText px-4">
-        <Navbar />
+      <Navbar />
       <div className="max-w-screen-xl mx-auto">
         <Banner />
         <About />
         <Resume />
-        <Projects/>
-        <Contact/>
+        <Projects />
+        <Contact />
+        <Footer />
       </div>
     </div>
   );
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+const Footer = () => {
+  return (
+    <section
+      id="projects"
+      className="mx-28 flex justify-center flex-col py-20 w-90 border-b-[1px] border-b-black"
+    >
+      <div className="flex justify-center items-center text-center">
+        <p>&copy; 2023. All rights reserved by Wubeshet Yimam</p>
+      </div>
+    </section>
+  );
+};
+
+export default Footer;
